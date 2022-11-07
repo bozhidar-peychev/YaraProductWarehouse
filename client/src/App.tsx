f6c413d2b77fa04ce7f19a7aa8ae059f1ddae9a1
Bozhidar Peychev <bozhidar.peychev.00@gmail.com>
2022-11-07 03:09:36 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,20 @@
-import './App.css';
-
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import DisplayData from './DisplayData';
 
 function App() {
-	const client = new ApolloClient({
-		cache: new InMemoryCache(),
-		uri: 'http://localhost:4000/graphql',
-	});
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'http://localhost:4000/graphql',
+  });
 
-	return (
-		<ApolloProvider client={client}>
-			<div className='App'>
-				<DisplayData />
-			</div>
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <DisplayData />
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
