2f1d65c5926263394046d337e1b3d82330b3fadf
Bozhidar Peychev <bozhidar.peychev.00@gmail.com>
2022-11-07 03:07:51 +0200

    BD to FD Connection

--- client/src/DisplayData.tsx
+++ client/src/DisplayData.tsx
@@ -1,36 +1,27 @@
-import { gql, useQuery } from '@apollo/client';
-
-const QUERY_ALL_BOOKS = gql`
-	query GetBooks {
-		books {
-			title
-			author
-		}
-	}
-`;
+import { useGetAllBooksQuery } from './generated/graphql';
 
 function DisplayData() {
-	const { data, loading } = useQuery(QUERY_ALL_BOOKS);
+  const { data, loading } = useGetAllBooksQuery();
 
-	if (loading) {
-		return <h1> DATA IS LOADING...</h1>;
-	}
+  if (loading) {
+    return <h1> DATA IS LOADING...</h1>;
+  }
 
-	return (
-		<div>
-			{data &&
-				data.books.map(
-					(book: { title: string; author: string }, index: number) => {
-						return (
-							<div key={index}>
-								<h1>Title: {book.title}</h1>
-								<h1>Author: {book.author}</h1>
-							</div>
-						);
-					}
-				)}
-		</div>
-	);
+  return (
+    <div>
+      {data &&
+        data.books?.map((book, index) => {
+          console.log({ book });
+
+          return (
+            <div key={index}>
+              <h1>Title: {book?.title}</h1>
+              <h1>Author: {book?.author?.username}</h1>
+            </div>
+          );
+        })}
+    </div>
+  );
 }
 
 export default DisplayData;
